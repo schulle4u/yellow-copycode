adacf29c0ce25ca779ea82b4e0670e6351ea9b4c
Robert Riebisch <15858666+bttrx@users.noreply.github.com>
2025-12-21 23:45:40 +0100

    Update copycode.php
    Fix coding standards

--- copycode.php
+++ copycode.php
@@ -36,10 +36,10 @@ class YellowCopycode {
     // Handle page output data
     public function onParsePageOutput($page, $text) {
         $output = null;
-        if ($text)  {
+        if ($text) {
             $outputNew = "</pre>\n";
-            $outputNew .= "<div class=\"copycode\"><button class=\"copycode-btn\" data-copycode-copied=\"".$this->yellow->language->getTextHtml("copycodeButtonCopied")."\">";
-            $outputNew .= "<span class=\"copycode-btn-text\">".$this->yellow->language->getTextHtml("copycodeButton")."</span>";
+            $outputNew .= "<div class=\"copycode\"><button class=\"copycode-btn\" data-copycode-copied=\"".$this->yellow->language->getTextHtml("CopycodeButtonCopied")."\">";
+            $outputNew .= "<span class=\"copycode-btn-text\">".$this->yellow->language->getTextHtml("CopycodeButton")."</span>";
             $outputNew .= "</button></div>\n";
             $output = preg_replace("/<\/pre>/", $outputNew, $text);
         }
